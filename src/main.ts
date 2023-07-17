26426c27a1a4b987de1bcd6098f60327c314e24c
Ramazan <r.tokoshev@gmail.com>
2023-07-17 18:24:25 +0600

    init: start

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({ credentials: false, origin: false });
+
+  await app.listen(7777);
+}
+bootstrap();
